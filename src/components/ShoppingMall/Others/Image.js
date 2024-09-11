31cab1634d4d7d4a9781cb4c7155f8623da85f85
Suraj Adhikari <sssadhikari222@gmail.com>
2024-09-11 06:34:21 +0545

    Multiple Image and Form

--- src/components/ShoppingMall/Others/Image.js
+++ src/components/ShoppingMall/Others/Image.js
@@ -4,10 +4,10 @@ const Image = ({ formData, setFormData }) => {
     const imageInputRef = useRef(null);
 
     const handleImageRemove = (index) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            images: prevFormData.images.filter((img, i) => i !== index),
-        }));
+        setFormData({
+            ...formData,
+            images: formData.images.filter((img, i) => i !== index),
+        });
     };
 
     const handleImageChange = (e) => {
@@ -20,8 +20,12 @@ const Image = ({ formData, setFormData }) => {
             const imageUrl = URL.createObjectURL(file);
             images.push({ file, filename, imageUrl });
         }
-        setFormData((prevFormData) => ({ ...prevFormData, images: [...prevFormData.images, ...images] }));
+        setFormData({
+            ...formData,
+            images: [...formData.images, ...images]
+        });
     };
+
     return (
         <>
             {formData.images.length > 0 && (
@@ -35,33 +39,28 @@ const Image = ({ formData, setFormData }) => {
                                 </svg>
                             </span>
                         </div>
-                    )
-                    )}
+                    ))}
                 </div>
             )}
+
             <div className="relative z-0 w-full mb-5 group">
 
                 <div className="flex items-center justify-center w-full">
                     <label htmlFor="image" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
                             <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                             </svg>
                             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                             <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                         </div>
-                        <input id="image" type="file" name="image" className="hidden" ref={imageInputRef} onChange={handleImageChange} multiple />
+                        <input id="image" accept="image/*" name="image" multiple type="file" className="hidden" ref={imageInputRef} onChange={handleImageChange} />
                     </label>
                 </div>
-                <label
-                    htmlFor="image"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                    Image
-                </label>
+                <label htmlFor="image" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Image</label>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Image
+export default Image;
--- src/pages/ShoppingMall/Admin/Malls/Add.js
+++ src/pages/ShoppingMall/Admin/Malls/Add.js
@@ -9,93 +9,144 @@ import Swal from 'sweetalert2';
 import Image from '../../../../components/ShoppingMall/Others/Image';
 
 function Add() {
-    const [formData, setFormData] = useState({
+    const [formList, setFormList] = useState([{
         name: '',
         location: '',
         openingHours: '',
         images: [],
         imageURL: '',
-    });
+    }]);
+    
     const mallsCollectionRef = collection(db, "malls");
     const navigate = useNavigate();
 
-    const handleChange = (e) => {
+    const handleChange = (e, index) => {
         const { name, value } = e.target;
-        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+        const updatedFormList = [...formList];
+        updatedFormList[index][name] = value;
+        setFormList(updatedFormList);
     };
 
-      const handleSubmit = async (event) => {
+    const handleAddForm = () => {
+        setFormList([...formList, { name: '', location: '', openingHours: '', images: [], imageURL: '' }]);
+    };
+
+    const handleRemoveForm = (index) => {
+        const updatedFormList = [...formList];
+        updatedFormList.splice(index, 1);
+        setFormList(updatedFormList);
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const imageArrays = formData.images.map((image) => {
-          const imageRef = ref(storage, `malls/${image.filename}`);
-          return uploadBytes(imageRef, image.file).then((uploadResult) => {
-            return getDownloadURL(uploadResult.ref);
-          });
+        const uploadPromises = formList.map(async (formData) => {
+            const imageArrays = formData.images.map((image) => {
+                const imageRef = ref(storage, `malls/${image.filename}`);
+                return uploadBytes(imageRef, image.file).then((uploadResult) => {
+                    return getDownloadURL(uploadResult.ref);
+                });
+            });
+            const downloadUrls = await Promise.all(imageArrays);
+            const openingHours = moment(formData.openingHours, 'HH:mm').format('hh:mm A');
+            return addDoc(mallsCollectionRef, {
+                name: formData.name,
+                location: formData.location,
+                opening_hours: openingHours,
+                images: downloadUrls,
+            });
         });
-        const downloadUrls = await Promise.all(imageArrays);
-        const openingHours = moment(formData.openingHours, 'HH:mm').format('hh:mm A');
-        await addDoc(mallsCollectionRef, {
-          name: formData.name,
-          location: formData.location,
-          opening_hours: openingHours,
-          images: downloadUrls,
-        });
-        Swal.fire('Success!', 'Malls Successfully Added', 'success');
+
+        await Promise.all(uploadPromises);
+        Swal.fire('Success!', 'All Malls Successfully Added', 'success');
         navigate('/shopping-mall/admin/malls', { replace: true });
-      };
+    };
 
     return (
         <>
             <AdminLayout>
                 <h3 className="text-3xl font-bold dark:text-white mb-6">Add Shopping Mall</h3>
                 <form className="mx-auto" onSubmit={handleSubmit}>
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder=" "
-                            required
-                            value={formData.name}
-                            onChange={handleChange}
-                        />
-                        <label
-                            htmlFor="name"
-                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                        >
-                            Name
-                        </label>
-                    </div>
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input
-                            type="text"
-                            name="location"
-                            id="location"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                            placeholder=" "
-                            required
-                            value={formData.location}
-                            onChange={handleChange}
-                        />
-                        <label
-                            htmlFor="location"
-                            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                        >
-                            Location
-                        </label>
-                    </div>
+                    {formList.map((formData, index) => (
+                        <div key={index} className="mb-8 border-b pb-4">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" "
+                                    required
+                                    value={formData.name}
+                                    onChange={(e) => handleChange(e, index)}
+                                />
+                                <label
+                                    htmlFor="name"
+                                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                    Name
+                                </label>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input
+                                    type="text"
+                                    name="location"
+                                    id="location"
+                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" "
+                                    required
+                                    value={formData.location}
+                                    onChange={(e) => handleChange(e, index)}
+                                />
+                                <label
+                                    htmlFor="location"
+                                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                    Location
+                                </label>
+                            </div>
 
-                    <div className="relative z-0 w-full mb-5 group">
-                        <input type="time" name='openingHours' placeholder='time' id="time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value={formData.openingHours} onChange={handleChange} required />
-                        <label htmlFor="time" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Select time:</label>
-                    </div>
-                    <Image formData={formData} setFormData={setFormData} />
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input 
+                                    type="time" 
+                                    name='openingHours' 
+                                    id="time" 
+                                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                                    value={formData.openingHours} 
+                                    onChange={(e) => handleChange(e, index)} 
+                                    required 
+                                />
+                                <label htmlFor="time" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Select time:</label>
+                            </div>
+
+                            <Image formData={formData} setFormData={(updatedData) => {
+                                const updatedFormList = [...formList];
+                                updatedFormList[index] = updatedData;
+                                setFormList(updatedFormList);
+                            }} />
+
+                            {formList.length > 1 && (
+                                <button
+                                    type="button"
+                                    onClick={() => handleRemoveForm(index)}
+                                    className="text-red-500 hover:text-red-700 mt-2"
+                                >
+                                    Remove
+                                </button>
+                            )}
+                        </div>
+                    ))}
+                    <button
+                        type="button"
+                        onClick={handleAddForm}
+                        className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mb-4"
+                    >
+                        Append
+                    </button>
                     <button
                         type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Submit
+                        Submit All
                     </button>
                 </form>
             </AdminLayout>
@@ -103,4 +154,4 @@ function Add() {
     );
 }
 
-export default Add
+export default Add;
